de4d7a08b217073d516bdaf653ebe3ee16b7ee92
dlt-v <tmm.michalski@gmail.com>
2023-12-03 20:14:40 +0100

    Initialise Spring Boot

---
+++ server/src/main/java/app/tea_tracker/api/TeaTrackerApplication.java
@@ -0,0 +1,13 @@
+package app.tea_tracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaTrackerApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/app/tea_tracker/api/TeaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package app.tea_tracker.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
